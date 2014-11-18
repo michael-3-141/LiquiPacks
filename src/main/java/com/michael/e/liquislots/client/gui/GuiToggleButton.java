fef3358f7d48fc12461a258244bf0616a4ebbac8
michael-elgavi <michael.elgavi@gmail.com>
2014-11-18 22:17:56 +0200

    Almost done with rewritten GuiTankOptions

--- src/main/java/com/michael/e/liquislots/client/gui/GuiToggleButton.java
+++ src/main/java/com/michael/e/liquislots/client/gui/GuiToggleButton.java
@@ -1,5 +1,6 @@
 package com.michael.e.liquislots.client.gui;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 
 public class GuiToggleButton extends GuiButton{
@@ -7,14 +8,16 @@ public class GuiToggleButton extends GuiButton{
     private String textA;
     private String textB;
 
-    public GuiToggleButton(int id, int x, int y, int width, int height, String textA, String textB) {
-        super(id, x, y, width, height, textA);
+    public GuiToggleButton(int id, int x, int y, int height, String textA, String textB) {
+        super(id, x, y, 5, height, textA);
         this.textA = textA;
         this.textB = textB;
+        this.width = Minecraft.getMinecraft().fontRenderer.getStringWidth(displayString) + 10;
     }
 
     public void actionPerfomed(){
         this.displayString = this.displayString == textA ? textB : textA;
+        this.width = Minecraft.getMinecraft().fontRenderer.getStringWidth(displayString) + 10;
     }
 
     public boolean getState(){
--- src/main/java/com/michael/e/liquislots/common/GuiHandler.java
+++ src/main/java/com/michael/e/liquislots/common/GuiHandler.java
@@ -1,5 +1,6 @@
 package com.michael.e.liquislots.common;
 
+import com.michael.e.liquislots.Liquislots;
 import com.michael.e.liquislots.block.TileEntityLiquipackIO;
 import com.michael.e.liquislots.block.TileEntityLiquipackWorkbench;
 import com.michael.e.liquislots.client.gui.GuiLiquipackWorkbench;
@@ -9,8 +10,13 @@ import com.michael.e.liquislots.common.container.ContainerLiquipackBucketOptions
 import com.michael.e.liquislots.common.container.ContainerLiquipackIO;
 import com.michael.e.liquislots.common.container.ContainerLiquipackWorkbench;
 import com.michael.e.liquislots.common.container.ContainerPlayerTanks;
+import com.michael.e.liquislots.item.ItemLiquipackBucket;
+import com.michael.e.liquislots.network.message.ChangeLiquipackIOOptionsMessageHandler;
+import com.michael.e.liquislots.network.message.ChangeTankOptionsMessageHandler;
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 public class GuiHandler implements IGuiHandler{
@@ -38,10 +44,12 @@ public class GuiHandler implements IGuiHandler{
             case 0:
                 return new GuiPlayerTanks(player);
             case 1:
-                if(world.getTileEntity(x, y, z) instanceof TileEntityLiquipackIO)
-                return new GuiTankOptions(player, (TileEntityLiquipackIO) world.getTileEntity(x, y, z));
+                if(world.getTileEntity(x, y, z) instanceof TileEntityLiquipackIO) {
+                    TileEntityLiquipackIO te = (TileEntityLiquipackIO) world.getTileEntity(x, y, z);
+                    return new GuiTankOptions(player, new GuiModeIO(te), new ContainerLiquipackIO(player, te));
+                }
             case 2:
-                return new GuiTankOptions(player, player.getHeldItem());
+                return new GuiTankOptions(player, new GuiModeLiquipackBucket(player.getHeldItem()), new ContainerLiquipackBucketOptions(player, player.getHeldItem()));
             case 3:
                 if(world.getTileEntity(x, y, z) instanceof TileEntityLiquipackWorkbench)
                     return new GuiLiquipackWorkbench((TileEntityLiquipackWorkbench) world.getTileEntity(x, y, z), player);
@@ -50,4 +58,74 @@ public class GuiHandler implements IGuiHandler{
         }
     }
 
+    public static class GuiModeIO extends GuiTankOptions.GuiMode{
+
+        private TileEntityLiquipackIO te;
+
+        public GuiModeIO(TileEntityLiquipackIO te) {
+            super("Drain Liquipack", "Fill Liquipack");
+            this.te = te;
+        }
+
+        @Override
+        public void actionPerformed() {
+            Liquislots.INSTANCE.netHandler.sendToServer(new ChangeLiquipackIOOptionsMessageHandler.ChangeLiquipackIOOptionsMessage(te.getTank(), te.isDrainingMode()));
+        }
+
+        @Override
+        public int getTank() {
+            return te.getTank();
+        }
+
+        @Override
+        public void setTank(int tank) {
+            te.setTank(tank);
+        }
+
+        @Override
+        public boolean isDrainingMode() {
+            return te.isDrainingMode();
+        }
+
+        @Override
+        public void setDrainingMode(boolean drainingMode) {
+            te.setDrainingMode(drainingMode);
+        }
+    }
+
+    public static class GuiModeLiquipackBucket extends GuiTankOptions.GuiMode{
+
+        private ItemStack stack;
+
+        public GuiModeLiquipackBucket(ItemStack stack) {
+            super(StatCollector.translateToLocal("liquipackbucket.btnToggle.1"), StatCollector.translateToLocal("liquipackbucket.btnToggle.0"));
+            this.stack = stack;
+        }
+
+        @Override
+        public void actionPerformed() {
+            Liquislots.INSTANCE.netHandler.sendToServer(new ChangeTankOptionsMessageHandler.ChangeTankOptionsMessage(ItemLiquipackBucket.getSelectedTank(stack), ItemLiquipackBucket.isDrainingMode(stack)));
+        }
+
+        @Override
+        public int getTank() {
+            return ItemLiquipackBucket.getSelectedTank(stack);
+        }
+
+        @Override
+        public void setTank(int tank) {
+            ItemLiquipackBucket.setSelectedTank(stack, tank);
+        }
+
+        @Override
+        public boolean isDrainingMode() {
+            return ItemLiquipackBucket.isDrainingMode(stack);
+        }
+
+        @Override
+        public void setDrainingMode(boolean drainingMode) {
+            ItemLiquipackBucket.setDrainingMode(stack, drainingMode);
+        }
+
+    }
 }
