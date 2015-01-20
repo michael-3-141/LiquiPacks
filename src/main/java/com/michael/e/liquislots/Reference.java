a3cf8ebf62bd0cf2c275a464769a112e0aafebde
michael-elgavi <michael.elgavi@gmail.com>
2015-01-20 15:04:08 +0200

    buildscript now replaces version and uploads to curseforge

--- src/main/java/com/michael/e/liquislots/Reference.java
+++ src/main/java/com/michael/e/liquislots/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 
     public static final String MOD_ID = "liquislots";
     public static final String NAME = "Liquipacks";
-    public static final String VERSION = "1.7.10-1.3.2";
+    public static final String VERSION = "@MOD-VERSION@-@MC-VERSION@";
 }
